15d9659dc707c76ba4b5da044f124cab318ce8e3
Inês Gomes <72618166+Inesg16@users.noreply.github.com>
2024-05-14 11:40:09 +0100

    Changed script IP to amazon instance

--- blurimage.sh
+++ blurimage.sh
@@ -9,7 +9,7 @@ base64 $1 > temp.txt
 echo -e "data:image/jpg;base64,$(cat temp.txt)" > temp.txt               
 
 # Send the request.
-curl -X POST http://127.0.0.1:8000/blurimage --data @"./temp.txt" > result.txt   
+curl -X POST http://13.53.82.36:8000/blurimage --data @"./temp.txt" > result.txt   
 
 # Remove a formatting string (remove everything before the comma).
 sed -i 's/^[^,]*,//' result.txt
