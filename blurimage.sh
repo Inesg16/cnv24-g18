5174710bc17540c1e69a06d108b038b934927d0c
Inês Gomes <72618166+Inesg16@users.noreply.github.com>
2024-05-14 16:11:49 +0100

    blurimage.sh curl commands

--- blurimage.sh
+++ blurimage.sh
@@ -9,7 +9,11 @@ base64 $1 > temp.txt
 echo -e "data:image/jpg;base64,$(cat temp.txt)" > temp.txt               
 
 # Send the request.
-curl -X POST http://13.53.82.36:8000/blurimage --data @"./temp.txt" > result.txt   
+#curl -X POST http://localhost:8000/blurimage --data @"./temp.txt" > result.txt  
+curl -X POST http://13.60.68.14:8000/blurimage --data @"./temp.txt" > result.txt   
+#seq 1 200 | xargs -n1 -P10 curl -X POST http://proj-lb-1519616844.eu-north-1.elb.amazonaws.com/blurimage --data @"./temp.txt" > result.txt   
+#curl -X POST http://proj-lb-1519616844.eu-north-1.elb.amazonaws.com/blurimage --data @"./temp.txt" > result.txt
+#ab -c 1000 -n 5000 -p temp.txt -T "multipart/form-data; boundary=1234567890" http://proj-lb-1519616844.eu-north-1.elb.amazonaws.com/blurimage
 
 # Remove a formatting string (remove everything before the comma).
 sed -i 's/^[^,]*,//' result.txt
