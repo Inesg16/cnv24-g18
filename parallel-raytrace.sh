00ab45372f8404da599d6dd715019a9ed57a27d0
Inês Gomes <72618166+Inesg16@users.noreply.github.com>
2024-05-17 16:01:54 +0100

    READMEs and scripts updated for checkpoint submission

---
+++ parallel-raytrace.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+# Give arguments $1 as scene.txt
+for i in 1 2 3
+do
+# Add scene.txt raw content to JSON.
+cat $1 | jq -sR '{scene: .}' > payload$i.json
+
+# Add texmap.bmp binary to JSON (optional step, required only for some scenes).
+hexdump -ve '1/1 "%u\n"' texmap.bmp | jq -s --argjson original "$(<payload$i.json)" '$original * {texmap: .}' > payload$i.json
+done
+
+for i in 1 2 3
+do
+# Send the request.
+# Local
+curl -X POST http://127.0.0.1:8000/raytracer?scols=400\&srows=300\&wcols=400\&wrows=300\&coff=0\&roff=0\&aa=false --data @"./payload$i.json" > result.txt &
+# Using AWS
+#curl -X POST http://proj-lb-1519616844.eu-north-1.elb.amazonaws.com/raytracer?scols=400\&srows=300\&wcols=400\&wrows=300\&coff=0\&roff=0\&aa=false --data @"./payload.json" > result.txt
+done
+
+for i in 1 2 3
+do
+# Remove a formatting string (remove everything before the comma).
+sed -i 's/^[^,]*,//' result$i.txt
+
+tmp=$1
+a=${tmp%.*}
+
+# Decode from Base64.
+base64 -d result$i.txt > $a-result$i.bmp
+
+# Remove temporary files
+rm -f payload$i.json result$i.txt
+done
--- raytrace.sh
+++ raytrace.sh
@@ -1,32 +1,27 @@
 #!bin/bash
 
 # Give arguments $1 as scene.txt
-for i in 1 2 3
-do
+
 # Add scene.txt raw content to JSON.
-cat $1 | jq -sR '{scene: .}' > payload$i.json
+cat $1 | jq -sR '{scene: .}' > payload.json
 
 # Add texmap.bmp binary to JSON (optional step, required only for some scenes).
-hexdump -ve '1/1 "%u\n"' texmap.bmp | jq -s --argjson original "$(<payload$i.json)" '$original * {texmap: .}' > payload$i.json
-done
+hexdump -ve '1/1 "%u\n"' texmap.bmp | jq -s --argjson original "$(<payload.json)" '$original * {texmap: .}' >     payload.json
 
-for i in 1 2 3
-do
 # Send the request.
-curl -X POST http://127.0.0.1:8000/raytracer?scols=400\&srows=300\&wcols=400\&wrows=300\&coff=0\&roff=0\&aa=false --data @"./payload$i.json" > result.txt &
-done
+# Local
+curl -X POST http://127.0.0.1:8000/raytracer?scols=400\&srows=300\&wcols=400\&wrows=300\&coff=0\&roff=0\&aa=false --data @"./payload.json" > result.txt
+# Using AWS
+#curl -X POST http://proj-lb-1519616844.eu-north-1.elb.amazonaws.com/raytracer?scols=400\&srows=300\&wcols=400\&wrows=300\&coff=0\&roff=0\&aa=false --data @"./payload.json" > result.txt
 
-for i in 1 2 3
-do
 # Remove a formatting string (remove everything before the comma).
-sed -i 's/^[^,]*,//' result$i.txt
+sed -i 's/^[^,]*,//' result.txt
 
 tmp=$1
 a=${tmp%.*}
 
 # Decode from Base64.
-base64 -d result$i.txt > $a-result$i.bmp
+base64 -d result.txt > $a-result.bmp
 
 # Remove temporary files
-rm -f payload$i.json result$i.txt
-done
+rm -f payload.json result.txt
