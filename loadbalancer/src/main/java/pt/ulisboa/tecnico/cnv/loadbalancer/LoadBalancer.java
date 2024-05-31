00169549976e3e096a0ad8602aa9d909b9f64602
Inês Gomes <72618166+Inesg16@users.noreply.github.com>
2024-05-31 01:35:43 +0100

    Update LoadBalancer.java

--- loadbalancer/src/main/java/pt/ulisboa/tecnico/cnv/loadbalancer/LoadBalancer.java
+++ loadbalancer/src/main/java/pt/ulisboa/tecnico/cnv/loadbalancer/LoadBalancer.java
@@ -191,7 +191,7 @@ public class LoadBalancer {
     System.out.println("Forwarding to worker " + workerIP);
 
     HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("http://" + WORKER_IP + ":" + WORKER_PORT + "/" + "requestType"))
+        .uri(URI.create("http://" + WORKER_IP + ":" + WORKER_PORT + "/" + "blurimage"))
         .header("Content-Type", "application/x-www-form-urlencoded") // Use the appropriate content type
         .POST(HttpRequest.BodyPublishers.ofString(requestPayload))
         .build();
